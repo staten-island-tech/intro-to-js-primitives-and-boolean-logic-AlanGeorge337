854654c763fac3448208f1f23780432a708a3bec
AlanGeorge337 <56314602+AlanGeorge337@users.noreply.github.com>
2020-04-02 09:27:40 -0400

    1st commit

---
+++ app.js
@@ -0,0 +1,56 @@
+console.log("Connected");
+
+let string = "I am a String! ";
+///let sentence = "Hello Angel " + "Hello Vicky";
+///let combine = string + sentence;
+
+///Template Integrals
+
+let student = "Sandra";
+let student2 = "Gia";
+let courseName = "AP CSP";
+let teacherName = "Whalen";
+let sentence = student + "and " + student2 + "are " + "taking " + courseName + "with " + teacherName;
+
+let betterSentence = '${student} and ${student2} are taking ${courseName} with ${teacherName} next year';
+
+
+///Numbers and Integers
+
+let year = 2020;
+let nextYear = year + 1;
+let past = year / 2;
+let future = year * 2;
+
+///Booleans
+
+let trueStatement = true;
+///let trueStatement = false;
+
+///Null and Undefined
+
+// let richard; ================>>>> ///Lead to Undefined message on Console. (There is no valvue for richard.)
+
+// jacob ==========>>>> ///Leads to Not Defined message. (Jacob not even declared)
+
+///Boolean Logic: if/else Statements
+
+// let age = 17;
+let baoSquared = "Bao Bao";
+    console.log(age === "17");
+
+// || ======>>>> or
+// && ======>>>> and 
+
+
+let age = 17;
+if (age < 18){
+    console.log("You're a child...")
+}
+    else if( age === 18){
+        console.log("Happy Birthday!")
+    }
+        else{
+            console.log("Your're over 18...")
+        }
+
